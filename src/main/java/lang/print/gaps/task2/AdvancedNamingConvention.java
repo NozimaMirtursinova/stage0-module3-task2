a763ed92cef308cd417966e6d1467dfb3dba7c34
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 15:50:47 +0300

    Fix test

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,17 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+
+
+    private int age;
+    private int phoneNumber;
+
+    void callToFriend() {
+        int myFriend = 34;
+    }
+
+    void callByNumber(int number) {
+        int myNumber = 23;
+    }
+}
